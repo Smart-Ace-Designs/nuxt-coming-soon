77b0fa08aa61bf458105d606e592746915c63f7c
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-16 18:04:11 -0500

    Move logo section to component

---
+++ app/components/App/Logo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mb-12 flex items-center justify-center gap-x-3">
+    <div
+      class="flex size-12 items-center justify-center rounded-[14px] bg-linear-to-br from-theme-accent to-theme-accent-light text-xl"
+    >
+      <Icon name="fa6-solid:bolt"></Icon>
+    </div>
+    <span class="text-[2rem] font-bold tracking-widest uppercase">Flux</span>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,17 +17,8 @@ const { days, hours, minutes, seconds } = useDateCountdown(target);
   </div>
 
   <main class="w-full max-w-162.5 p-8 text-center">
-    <div class="mb-12 flex items-center justify-center gap-x-3">
-      <div
-        class="flex size-12 items-center justify-center rounded-[14px] bg-linear-to-br from-theme-accent to-theme-accent-light text-xl"
-      >
-        <Icon name="fa6-solid:bolt"></Icon>
-      </div>
-      <span class="text-[2rem] font-bold tracking-widest uppercase">Flux</span>
-    </div>
-
+    <AppLogo />
     <AppHeroSection />
-
     <ClientOnly>
       <section class="mb-12 flex items-center justify-center gap-x-4">
         <AppTimeBlock label="Days" :value="days" />
