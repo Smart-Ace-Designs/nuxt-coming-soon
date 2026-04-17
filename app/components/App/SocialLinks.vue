186cadfdd137523964d0413fde2fb86059f2cf04
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-16 21:56:29 -0500

    Update hover for social media icons

--- app/components/App/SocialLinks.vue
+++ app/components/App/SocialLinks.vue
@@ -4,19 +4,19 @@
   <div class="mt-8 flex justify-center gap-x-4">
     <a
       href="#"
-      class="flex size-11 items-center justify-center rounded-full border border-[rgba(255,255,255,0.1)] bg-[rgba(255,255,255,0.05)] text-[1.1rem] text-theme-text-dim transition duration-300 hover:text-theme-text"
+      class="flex size-11 items-center justify-center rounded-full border border-[rgba(255,255,255,0.1)] bg-[rgba(255,255,255,0.05)] text-[1.1rem] text-theme-text-dim transition duration-300 hover:border-theme-accent hover:bg-theme-accent hover:text-theme-text"
     >
       <Icon name="fa6-brands:x-twitter"></Icon>
     </a>
     <a
       href="#"
-      class="flex size-11 items-center justify-center rounded-full border border-[rgba(255,255,255,0.1)] bg-[rgba(255,255,255,0.05)] text-[1.1rem] text-theme-text-dim transition duration-300 hover:text-theme-text"
+      class="flex size-11 items-center justify-center rounded-full border border-[rgba(255,255,255,0.1)] bg-[rgba(255,255,255,0.05)] text-[1.1rem] text-theme-text-dim transition duration-300 hover:border-theme-accent hover:bg-theme-accent hover:text-theme-text"
     >
       <Icon name="fa6-brands:instagram"></Icon>
     </a>
     <a
       href="#"
-      class="flex size-11 items-center justify-center rounded-full border border-[rgba(255,255,255,0.1)] bg-[rgba(255,255,255,0.05)] text-[1.1rem] text-theme-text-dim transition duration-300 hover:text-theme-text"
+      class="flex size-11 items-center justify-center rounded-full border border-[rgba(255,255,255,0.1)] bg-[rgba(255,255,255,0.05)] text-[1.1rem] text-theme-text-dim transition duration-300 hover:border-theme-accent hover:bg-theme-accent hover:text-theme-text"
     >
       <Icon name="fa6-brands:discord"></Icon>
     </a>
