555690ecf06c06b3458dbf7a3c2cea7c4ce1723e
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-21 20:12:58 -0500

    Convert from CRLF to LF

--- app/components/App/Blobs.vue
+++ app/components/App/Blobs.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="absolute inset-0 -z-10">
     <div
-      class="blob absolute -top-[15%] -left-[10%] size-125 rounded-full bg-linear-to-br from-theme-accent to-[#8b5cf6] opacity-60 blur-[80px]"
+      class="blob absolute top-[-15%] left-[-10%] size-125 rounded-full bg-linear-to-br from-theme-accent to-[#8b5cf6] opacity-60 blur-[80px]"
     ></div>
     <div
-      class="blob-2 blob absolute -right-[10%] -bottom-[10%] size-100 rounded-full bg-linear-to-br from-[#ec4489] to-[#8b5cf6] opacity-60 blur-[80px]"
+      class="blob-2 blob absolute right-[-10%] bottom-[-10%] size-100 rounded-full bg-linear-to-br from-[#ec4489] to-[#8b5cf6] opacity-60 blur-[80px]"
     ></div>
     <div
       class="blob-3 blob absolute top-1/2 left-1/2 size-75 rounded-full bg-linear-to-br from-[#06b6d4] to-theme-accent opacity-60 blur-[80px]"
